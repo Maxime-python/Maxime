280d8a3065160487aa2021cc5f9ba2ea3d30fcb3
Maxime-python <82325298+Maxime-python@users.noreply.github.com>
2021-04-11 16:38:24 +0200

    Add files via upload

---
+++ Hello World.py
@@ -0,0 +1,8 @@
+character_name = "Tom"
+character_specy = "human"
+print("There once was a " + character_specy + " who found a Rubik's Cube")
+print("His name was " + character_name + ".")
+print("But one day he met Peter, a (completely) crazy dude with awesome content.")
+character_specy = "duck"
+character_name = "Mark"
+print("So, Peter named him " + character_name + " and he became a " + character_specy + ".")
